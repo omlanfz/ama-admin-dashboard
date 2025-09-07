c4083210dfa30c4e6c8569d6c1f47ce808767af3
Tanjia Tasnim Kotha <tkotha83@gmail.com>
2025-09-07 21:24:38 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Dashboard from "./pages/Dashboard";
 import Orders from "./pages/Orders";
+import Camps from "./pages/Camps"; // 1. Import the new page
 import Statistics from "./pages/Statistics";
 import ControlPanel from "./pages/ControlPanel";
 import Account from "./pages/Account";
@@ -11,6 +12,7 @@ import Login from "./pages/Login";
 const tabs = {
   dashboard: <Dashboard />,
   orders: <Orders />,
+  camps: <Camps />, // 2. Add the new page to the tabs object
   statistics: <Statistics />,
   control: <ControlPanel />,
   account: <Account />,
